5aca4c54bde888710fd5ada765acfeebe57eaa69
Baodoan1812 <doanngocbaoltn@gmail.com>
2025-03-31 09:45:29 +0700

    hw

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import './build_container.dart';
-import './build_schedule.dart';
-import './build_task.dart';
+import './nutritional_box.dart';
+import './food_box.dart';
 void main() {
   runApp(const MainApp());
 }
@@ -54,71 +53,179 @@ class _MainAppState extends State<MainApp>{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home:  SafeArea(
-
-
         child: Scaffold(
-          floatingActionButton: Container(
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(100)
-            ),
 
-            child: InkWell(
-              onTap: () => setState(() {
-                _id = Random().nextInt(4);
-              }),
-              child: Center(
-                child: Text(
-                  "+",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 60,
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(80), // Increase height
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10), // Add padding
+              child: AppBar(
+                backgroundColor: Colors.white, // White background
+                elevation: 0, // Remove shadow
+                centerTitle: true,
+                title: Text(
+                  'Breakfast',
+                  style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                leading: CircleAvatar(
+                  backgroundColor: Colors.grey.shade200,
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.black),
+                    onPressed: () {
+                      // Handle back button
+                    },
                   ),
                 ),
+                actions: [
+                  CircleAvatar(
+                    backgroundColor: Colors.grey.shade200,
+                    child: IconButton(
+                      icon: Icon(Icons.share, color: Colors.black),
+                      onPressed: () {
+                        // Handle share button
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-            appBar: AppBar(
-              backgroundColor: Colors.blue,
-              title: Text(
-                'Day la title'
-              ),
-              centerTitle: true,
-              leading: Text('leading')
-
-            ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    DateCard(date: "15", day: "Mon",color:Colors.white),
-                    const SizedBox(width: 20),
-                    DateCard(date: "16", day: "Tue",color:Colors.white),
-                    const SizedBox(width: 20),
-                    DateCard(date: "17", day: "Wed",color: Colors.blue),
-                    const SizedBox(width: 20),
-                    DateCard(date: "18", day: "Thu",color:Colors.white),
-                  ],
-                 ),
-                const SizedBox(height: 30),
-                Row(
-                  // mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    EventCard(
-                      time: events[_id]["time"],
-                      period: events[_id]["session"],
-                      title: events[_id]["title"],
-                      description:events[_id]["description"],
+          body: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                  child: Column(
+                    children: [
+                  Container(
+                  decoration: BoxDecoration(
+                  color: Colors.white,
+                    border: Border(
+                      bottom: BorderSide(color: Colors.grey.shade300, width: 0), // Bottom border effect
                     ),
-                  ],
-                ),
+                  ),
+                              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20), // Left & right padding
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Breakfast',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Today',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey.shade600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add, size: 28, color: Colors.black),
+                    onPressed: () {
+                      // Handle add button press
+                    },
+                  ),
+                                ],
+                              ),
+                            ),
+                      Divider(
+                        height: 34,
+                        color: Colors.grey,
+                        thickness: 2.5,
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                RichText(
+                                  text: TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: "452",
+                                        style: TextStyle(
+                                          fontSize: 62,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: "kcal",
+                                        style: TextStyle(
+                                          fontSize: 40,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ],
 
-                  ],
+                                  ),
+                                ),
+                                Container(
+                                  height: 40,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.circular(20)
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "Normal",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                NutritionalBox(color: Colors.purpleAccent, name: "Protein", maxVal: 80, val: 13),
+                                NutritionalBox(color: Colors.orange, name: "Fat", maxVal: 60, val: 20),
+                                NutritionalBox(color: Colors.black87, name: "Carbs", maxVal: 200, val: 19),
+                              ],
+                            ),
+                          ],
+                        ),
+
+                      ),
+                      Column(
+                        children: [
+                          SizedBox(height: 15,),
+                          FoodBox(name: "Fried eggs", kcal: '378', protein: '12', fat: '17', carbs: '17', weight: '100g',),
+                          SizedBox(height: 15,),
+                          FoodBox(name: "Mug of coffee", kcal: '74', protein: '1', fat: '3', carbs: '2', weight: '450ml',),
+                          SizedBox(height: 15,),
+                          FoodBox(name: "Fried eggs", kcal: '378', protein: '12', fat: '17', carbs: '17', weight: '100g',),
+                          SizedBox(height: 15,),
+                          FoodBox(name: "Mug of coffee", kcal: '74', protein: '1', fat: '3', carbs: '2', weight: '450ml',),
+                          SizedBox(height: 15,),
+                          FoodBox(name: "Fried eggs", kcal: '378', protein: '12', fat: '17', carbs: '17', weight: '100g',),
+                          SizedBox(height: 15,),
+                          FoodBox(name: "Mug of coffee", kcal: '74', protein: '1', fat: '3', carbs: '2', weight: '450ml',),
+                          SizedBox(height: 15,),
+                          FoodBox(name: "Fried eggs", kcal: '378', protein: '12', fat: '17', carbs: '17', weight: '100g',),
+                          SizedBox(height: 15,),
+                          FoodBox(name: "Mug of coffee", kcal: '74', protein: '1', fat: '3', carbs: '2', weight: '450ml',),
+                          SizedBox(height: 15,),
+
+                        ],
+                      ),
+                    ],
+
+                  ),
                 ),
             ),
           ),
